aa9db12afedd76f172e8d6582fa8ab315a00763a
Myles McNamara <myles@smyl.es>
2014-04-09 18:01:46 -0400

    Update eventdata.js

--- eventdata.js
+++ eventdata.js
@@ -1,51 +1,47 @@
-$(document).ready(function() {
-	var dataURL = "http://home.d3sunguide.com/test/c2c/eventdata.xml";
-	$.get(dataURL, function(rawXML) {
+$(document).ready(function(){
+    var dataURL = "http://home.d3sunguide.com/test/c2c/eventdata.xml";
+    $.get(dataURL, function (rawXML) {
+        
 
 		var xml = $(rawXML).text();
-
-		$(xml).find('event').each(function() {
+		
+		
+		$(xml).find("event").each(function(){
+		
+		
 			var typeDesc = $(this).find('typeDesc').text();
 			var status = $(this).find('status').text();
 			var type = $(this).find('type')[0].attributes[0].name;
 			lastUpdated = moment($(this).find('updateTimestamp').text()).fromNow();
 			var lanesAffected = $(this).find('affectedLanes').text();
-
-			var types = {
-				vehiclecollision: 'Crash',
-				debris: 'Debris',
-				vehiclefire: 'Vehicle Fire',
-				stalledvehicle: 'Stalled Vehicle'
-			};
-
-			var importantEvents = ['vehiclecollision', 'vehiclefire'];
-			var invalidTypes = ['Closed', 'Weather', 'Visibility', 'Vehicle Alert'];
-
-			// Shorthand IF Statement
-			// ---------------------------------------
-			// (condition) ? doiftrue : doiffalse
-			// ---------------------------------------
-			(!types[type]) ? typeDesc = "Unknown Event" : typeDesc = types[type];
-
-			// Is the same as this:
-			// if ($types[type]){
-			// 	typeDesc = "Unknown";
-			// } else {
-			// 	typeDesc = types[type];
-			// }
-
-			// Check array values, will return index number if found, otherwise returns -1
-			if (invalidTypes.indexOf(typeDesc) == -1 || invalidTypes.indexOf(status) == -1) {
-				if (!lanesAffected) lanesAffected = "None";
-				var buildHTML = '<li class="title"> ' + typeDesc + '</li><li class="tvtData"> Last Updated: ' + lastUpdated + '</li><li class="tvtData"> Lanes Affected: ' + lanesAffected + '</li>';
-
-				if (importantEvents.indexOf(type) != -1) {
-					$('#eventdata').prepend(buildHTML);
-				} else {
-					$('#eventdata').append(buildHTML);
+			
+			
+			if ( type == "vehiclecollision" ) {
+				var typeDesc = "Crash";
+			}
+			if ( type == "debris" ) {
+				var typeDesc = "Debris";
+			}
+			if ( type == "vehiclefire" ) {
+				var typeDesc = "Vehicle Fire";
+			}
+			if ( type == "stalledvehicle" ) {
+				var typeDesc = "Stalled Vehicle";
+			}
+			
+			if ( !((status == 'Closed') || (typeDesc == 'Weather') || (typeDesc == 'Visibility') || (typeDesc == 'Vehicle Alert') )){
+				$('#eventdata').append('<li class="title"> ' + typeDesc + '</li>');
+				$('#eventdata').append('<li class="tvtData"> Last Updated: ' + lastUpdated + '</li>');
+				if(lanesAffected != ""){
+					$('#eventdata').append('<li class="tvtData"> Lanes Affected: ' + lanesAffected + '</li>');
+				}
+				if(lanesAffected == ""){
+					$('#eventdata').append('<li class="tvtData"> Lanes Affected: ' + lanesAffected + 'None</li>');
 				}
 			}
+			
+        });
+		
 
-		});
-	});
+    });
 });
