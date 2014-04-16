6ca4cc115d2f4228d44798ac54d04a9bcc48f98f
Myles McNamara <myles@smyl.es>
2014-04-15 21:21:44 -0400

    Update eventdata.js

--- eventdata.js
+++ eventdata.js
@@ -1,6 +1,12 @@
-var localeDataURL = "http://test.d3sunguide.com/c2c/localedata.xml";
-var eventDataURL = "http://test.d3sunguide.com/c2c/eventdata.xml";
-var processedEvents = [];
+/*
+ * @Author: Myles McNamara (myles@smyl.es)
+ * @Date:   2014-04-15 12:21:47
+ * @Last Modified 2014-04-15
+ * @Last Modified time: 2014-04-15 21:21:11
+ */
+var localeDataURL = "sample-localedata.xml";
+var eventDataURL = "raw-eventdata.xml";
+
 var possibleLanes = ['entranceRamp', 'hovLane', 'exitRamp', 'lane1', 'lane2', 'lane3', 'lane4', 'lane5', 'lane6', 'rightShoulder', 'leftShoulder'];
 var priorityLanes = ['entranceRamp', 'hovLane', 'exitRamp', 'lane1', 'lane2', 'lane3', 'lane4', 'lane5', 'lane6'];
 var shoulderLanes = ['rightShoulder', 'leftShoulder'];
@@ -24,51 +30,6 @@ var types = {
     stalledvehicle: 'Stalled Vehicle'
 };
 
-function processAffectedLanes(affectedLanesObj, callback) {
-        var eventPriority = 0;
-        var affectedLanes = [];
-
-        // Loop through each attribute under affectedLanes
-        $.each(affectedLanesObj.attributes, function(i, attrib) {
-            var laneAffected = attrib.name;
-            var isLaneAffected = attrib.value;
-
-            if(isLaneAffected == true && $.inArray(laneAffected, possibleLanes)){
-            	// Increase event priority for each lane blocked
-            	eventPriority += incEachAffectedLane;
-            	// Increase priority if lane is in priorityLanes array
-            	if ($.inArray(laneAffected, priorityLanes)) eventPriority += incPriorityLanes;
-            	affectedLanes.push(laneAffected);
-            }
-        });
-        // Callback after going through each attribute
-        callback(eventPriority, affectedLanes);
-}
-
-function filterConfirmedEvents(xml){
-    // Get only confirmed events, discard any others
-    var confirmedEvents = $(xml).find('event').filter(function() {
-        return $('status', this).text() == 'Confirmed';
-    });
-
-    return confirmedEvents;
-}
-
-function addEvent(eventID, lastUpdated, typeDesc, lat, lon, locationId, atisSeverity, processEventPriority, processAffectedLanes) {
-    var eventData = {
-        eventID: eventID,
-        lastUpdated: lasteUpdated,
-        typeDesc: typeDesc,
-        lat: lat,
-        lon: lon,
-        locationId: locationId,
-        atisSeverity: atisSeverity,
-        eventPriority: processEventPriority,
-        affectedLanes: processAffectedLanes
-    }
-    processedEvents.push(eventData);
-}
-
 $(document).ready(function() {
     $.get(eventDataURL, {}, function(rawXML) {
         // Get raw XML text from ajax response
@@ -78,8 +39,8 @@ $(document).ready(function() {
 
         // Loop through each confirmed event
         confirmedEvents.each(function() {
-			// Cache selector
-			var $event = $(this);
+            // Cache selector
+            var $event = $(this);
             processEvent($event);
         });
         // After processing all events, output event html
@@ -87,12 +48,73 @@ $(document).ready(function() {
     });
 });
 
-function processEvent(event) {
-	var processAffectedLanes = 'None';
-	var processEventPriority = 0;
+function outputEvents(events) {
+    console.dir(events);
+}
 
-	// Cache selector
-	var $event = event;
+/**
+ * Go through event affected lane data set priority, and add each affected lane to array
+ * @param  {object}   affectedLanesObj jQuery object
+ * @param  {Function} callback         Callback function
+ */
+function processAffectedLanes(alAttributes, callback) {
+    var eventPriority = 0;
+    var affectedLanes = [];
+
+    // Loop through each attribute under affectedLanes
+    $.each(alAttributes, function(i, attrib) {
+        var laneAffected = attrib.name;
+        var isLaneAffected = attrib.value;
+
+        if (isLaneAffected == true && $.inArray(laneAffected, possibleLanes)) {
+            // Increase event priority for each lane blocked
+            eventPriority += incEachAffectedLane;
+            // Increase priority if lane is in priorityLanes array
+            if ($.inArray(laneAffected, priorityLanes)) eventPriority += incPriorityLanes;
+            affectedLanes.push(laneAffected);
+        }
+    });
+    // Callback after going through each attribute
+    callback(eventPriority, affectedLanes);
+}
+
+/**
+ * Filter out only confirmed events
+ * @param  {XML} xml XML data
+ * @return {XML}     Filtered XML data
+ */
+function filterConfirmedEvents(xml) {
+    // Get only confirmed events, discard any others
+    var confirmedEvents = $(xml).find('event').filter(function() {
+        return $('status', this).text() == 'Confirmed';
+    });
+
+    return confirmedEvents;
+}
+
+var processedEvents = [];
+
+function addEvent(eventID, lastUpdated, typeDesc, lat, lon, locationId, atisSeverity, pEventPriority, pAffectedLanes) {
+    var eventData = {
+        eventID: eventID,
+        lastUpdated: lastUpdated,
+        typeDesc: typeDesc,
+        lat: lat,
+        lon: lon,
+        locationId: locationId,
+        atisSeverity: atisSeverity,
+        eventPriority: pEventPriority,
+        affectedLanes: pAffectedLanes
+    }
+    processedEvents.push(eventData);
+}
+
+function processEvent(event) {
+    var pAffectedLanes = 'None';
+    var pEventPriority = 0;
+
+    // Cache selector
+    var $event = event;
 
     // Event Type handling
     var type = $event.find('type')[0].attributes[0].name;
@@ -100,47 +122,47 @@ function processEvent(event) {
     // If current event does not have type defined above, set description to Unknown (shorthand IF statement)
     (!types[type]) ? typeDesc = "Event" : typeDesc = types[type];
 
-	// Omit processing invalid event types
+    // Omit processing invalid event types
     if ($.inArray(typeDesc, invalidTypes) == -1) {
-    	var eventID = $event.find('event[id]').text();
-    	// Location
-	    var lat = $event.find('lat').text();
-	    var lon = $event.find('lon').text();
-	    var locationId = $event.find('locationId').text();
-	    // Get and format last updated timestamp
-    	var lastUpdated = moment($event.find('updateTimestamp').text()).fromNow();
-    	// Severity
-    	var atisSeverity = $event.find('atisSeverity > severity');
-    	// Check if lanes are affected
-    	var affectedLanesObj = $event.find('affectedLanes');
-    	// Process affected lanes
-    	if(affectedLanesObj){
-			processAffectedLanes(affectedLanesObj, function(eventPriority, affectedLanes){
-				processAffectedLanes = affectedLanes;
-				processEventPriority = eventPriority;
-			});
-		}
-		// Increase priority if event is in priorityEventTypes array
-		if (priorityEventTypes.indexOf(type) != -1) processEventPriority += incPriorityEventTypes;
-		// Increase priority based on severity
-		if (atisSeverity == 'Major') processEventPriority += incMajorSeverity;
-		if (atisSeverity == 'Minor') processEventPriority += incMinorSeverity;
+        var eventID = $event.find('event[id]').text();
+        // Location
+        var lat = $event.find('lat').text();
+        var lon = $event.find('lon').text();
+        var locationId = $event.find('locationId').text();
+        // Get and format last updated timestamp
+        var lastUpdated = moment($event.find('updateTimestamp').text()).fromNow();
+        // Severity
+        var atisSeverity = $event.find('atisSeverity > severity').text();
+        // Check if lanes are affected
+        var alAttributes = $event.find('affectedLanes').attributes;
+        // Process affected lanes
+        if (alAttributes) {
+            processAffectedLanes(alAttributes, function(eventPriority, affectedLanes) {
+                pAffectedLanes = affectedLanes;
+                pEventPriority = eventPriority;
+            });
+        }
+        // Increase priority if event is in priorityEventTypes array
+        if (priorityEventTypes.indexOf(type) != -1) pEventPriority += incPriorityEventTypes;
+        // Increase priority based on severity
+        if (atisSeverity == 'Major') pEventPriority += incMajorSeverity;
+        if (atisSeverity == 'Minor') pEventPriority += incMinorSeverity;
 
-		// Add event to array
-		addEvent(eventID, lastUpdated, typeDesc, lat, lon, locationId, atisSeverity, processEventPriority, processAffectedLanes);
+        // Add event to array
+        addEvent(eventID, lastUpdated, typeDesc, lat, lon, locationId, atisSeverity, pEventPriority, pAffectedLanes);
     }
 
 }
 
-function getLocaleData(){
+function getLocaleData() {
     $.get(dataURL, {}, function(rawXML) {
         // Get raw XML text from ajax response
         var xml = $(rawXML).text();
 
     });
-        var buildHTML = '<li class="title"> ' + typeDesc + '</li>';
-        buildHTML += '<li class="tvtData"> Last Updated: ' + lastUpdated + '</li>';
-        buildHTML += '<li class="tvtData"> Lanes Affected: ' + lanesAffected + '</li>';
+    var buildHTML = '<li class="title"> ' + typeDesc + '</li>';
+    buildHTML += '<li class="tvtData"> Last Updated: ' + lastUpdated + '</li>';
+    buildHTML += '<li class="tvtData"> Lanes Affected: ' + lanesAffected + '</li>';
 }
 
 $(".refresh").click(function() {
